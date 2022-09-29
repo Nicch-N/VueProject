4193233016085cb1b7a1a818d8adfb09df168e9a
Nicch-N <Nicch-N@users.noreply.github.com>
2022-09-29 16:18:52 +0700

    Installed Vue

---
+++ vue-app/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+<style>
+    .navBar{
+        border-bottom: 1px solid rgba(163, 163, 163, 0.178);
+        display: flex;
+        padding: 10px 5px 10px 30px;
+        background: linear-gradient(rgb(255, 255, 255), rgb(241, 241, 241));;
+    }
+    *{
+        margin: 0px;
+        margin-top: 0px;
+    }
+    .navItem{
+        margin-right: 50px;
+        width: 50px;
+        display: flex;
+        text-align: center;
+        background: black;
+    }
+    .navLink{
+        text-align: center;
+    }
+    .navItem :hover{
+        font-style: italic;
+        background: blue;
+    }
+</style>
+<template>
+    <div class="navBar">
+        <div class="navItem">
+            <a class="navLink">A</a>
+        </div>
+        <div class="navItem">
+            <a class="navLink">B</a>
+        </div>
+        <div class="navItem">
+            <a class="navLink">C</a>
+        </div>
+    </div>
+</template>
